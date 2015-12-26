f8876385c794830d8f5ab804b11a09af8c68e18a
RichardMurphy <rickmurphy@outlook.com>
2015-12-26 13:42:39 +1100

    1st commit

---
+++ Full Screen Viewer/MainForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace JibbaJabba
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.mainImageOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBoxOpenFile = new System.Windows.Forms.PictureBox();
+            this.pictureBoxFullScreen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOpenFile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFullScreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.BackColor = System.Drawing.Color.Black;
+            this.mainPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(684, 332);
+            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.TabStop = false;
+            this.mainPictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mainPictureBox_MouseClick);
+            this.mainPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mainPictureBox_MouseDown);
+            this.mainPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainPictureBox_MouseMove);
+            this.mainPictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mainPictureBox_MouseUp);
+            // 
+            // mainImageOpenFileDialog
+            // 
+            this.mainImageOpenFileDialog.Filter = "bmp jpg gif png|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            this.mainImageOpenFileDialog.Title = "Open a Picture";
+            // 
+            // pictureBoxOpenFile
+            // 
+            this.pictureBoxOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxOpenFile.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxOpenFile.Image = global::JibbaJabba.Properties.Resources.open_file_emoji_small;
+            this.pictureBoxOpenFile.Location = new System.Drawing.Point(640, 41);
+            this.pictureBoxOpenFile.Name = "pictureBoxOpenFile";
+            this.pictureBoxOpenFile.Size = new System.Drawing.Size(38, 23);
+            this.pictureBoxOpenFile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxOpenFile.TabIndex = 4;
+            this.pictureBoxOpenFile.TabStop = false;
+            this.pictureBoxOpenFile.Click += new System.EventHandler(this.pictureBoxOpenFile_Click);
+            // 
+            // pictureBoxFullScreen
+            // 
+            this.pictureBoxFullScreen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxFullScreen.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxFullScreen.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxFullScreen.Image")));
+            this.pictureBoxFullScreen.Location = new System.Drawing.Point(652, 8);
+            this.pictureBoxFullScreen.Name = "pictureBoxFullScreen";
+            this.pictureBoxFullScreen.Size = new System.Drawing.Size(26, 23);
+            this.pictureBoxFullScreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxFullScreen.TabIndex = 5;
+            this.pictureBoxFullScreen.TabStop = false;
+            this.pictureBoxFullScreen.Click += new System.EventHandler(this.pictureBoxFullScreen_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(684, 332);
+            this.Controls.Add(this.pictureBoxFullScreen);
+            this.Controls.Add(this.pictureBoxOpenFile);
+            this.Controls.Add(this.mainPictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Picture Viewer";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOpenFile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFullScreen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainPictureBox;        
+        private System.Windows.Forms.OpenFileDialog mainImageOpenFileDialog;
+        private System.Windows.Forms.PictureBox pictureBoxOpenFile;
+        private System.Windows.Forms.PictureBox pictureBoxFullScreen;
+    }
+}
+
---
+++ Full Screen Viewer/MainForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+namespace JibbaJabba
+{
+    public partial class MainForm : Form
+    {
+        Image maimImage = JibbaJabba.Properties.Resources.my_logo;
+        bool mouseIsDown = false, fullScreenIsChecked = false, isDraging = false;
+        FormWindowState priorWindowState;
+        List<string> files;
+        int index;
+
+        Picture picture = new Picture();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            picture.Fit(maimImage, ref mainPictureBox);
+            this.mainPictureBox.Paint += new PaintEventHandler(mainImage_Paint);
+            mainPictureBox.Refresh();
+        }
+
+        private void mainPictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseIsDown = true;
+
+            MouseEventArgs mouse = e as MouseEventArgs;
+
+            picture.InitialiseMousePosition(mouse.X, mouse.Y);
+        }
+
+        private void mainPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            MouseEventArgs mouse = e as MouseEventArgs;
+
+            if (mouse.Button == MouseButtons.Left && mouseIsDown)
+            {
+                isDraging = true;
+                picture.Drag(maimImage, mouse, ref mainPictureBox);
+                mainPictureBox.Refresh();
+            }
+        }
+
+        private void mainPictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseIsDown = false;
+
+            if (isDraging)
+                isDraging = false;
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            MouseEventArgs mouse = e as MouseEventArgs;
+
+            picture.Zoom(maimImage, ref mainPictureBox, mouse);
+            mainPictureBox.Refresh();
+        }       
+
+        private void mainImage_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            e.Graphics.DrawImage(maimImage, picture.X, picture.Y, picture.Width, picture.Height); //System.OverflowException 
+        }        
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            picture.Fit(maimImage, ref mainPictureBox);
+            mainPictureBox.Refresh();
+        }  
+               
+        private void pictureBoxOpenFile_Click(object sender, EventArgs e)
+        {
+            if (mainImageOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                maimImage = Image.FromFile(mainImageOpenFileDialog.FileName);
+                picture.Fit(maimImage, ref mainPictureBox);
+
+                this.mainPictureBox.Paint += new PaintEventHandler(mainImage_Paint);
+                mainPictureBox.Refresh(); //calls MainImage_Paint
+
+                string path = Path.GetDirectoryName(mainImageOpenFileDialog.FileName);
+                files = Directory.EnumerateFiles(path, "*.*", SearchOption.TopDirectoryOnly)
+                    .Where(s => s.ToLower().EndsWith(".bmp") || s.ToLower().EndsWith(".jpg") || s.ToLower().EndsWith(".png")).ToList();
+
+                index = files.IndexOf((mainImageOpenFileDialog.FileName));
+            }
+        }       
+
+        private void mainPictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.X < mainPictureBox.Width / 2 && isDraging == false)
+            {
+                if (files != null)
+                {
+                    if (index > 0)
+                    {
+                        index -= 1;
+                        maimImage = Image.FromFile(files.ElementAt(index));
+                    }
+                    else if (index == 0)
+                    {
+                        index = files.Count - 1;
+                        maimImage = Image.FromFile(files.ElementAt(index));
+                    }
+
+                    picture.Fit(maimImage, ref mainPictureBox);
+                    mainPictureBox.Refresh();
+                }
+            }
+            else if (e.X > mainPictureBox.Width / 2 && isDraging == false)
+            {
+                if (files != null)
+                {
+                    if (index < files.Count - 1)
+                    {
+                        index += 1;
+                        maimImage = Image.FromFile(files.ElementAt(index));
+                    }
+                    else if (index == files.Count - 1)
+                    {
+                        index = 0;
+                        maimImage = Image.FromFile(files.ElementAt(index));
+                    }
+                    picture.Fit(maimImage, ref mainPictureBox);
+                    mainPictureBox.Refresh();
+                }
+            }
+        }
+
+        private void pictureBoxFullScreen_Click(object sender, EventArgs e)
+        {           
+            if (fullScreenIsChecked == false)
+            {
+                FormBorderStyle = FormBorderStyle.None;
+                priorWindowState = WindowState;
+
+                if (WindowState == FormWindowState.Maximized)
+                    WindowState = FormWindowState.Minimized;
+
+                WindowState = FormWindowState.Maximized;
+                fullScreenIsChecked = true;
+
+                pictureBoxFullScreen.Image = global::JibbaJabba.Properties.Resources.full_screen_off;
+            }
+            else
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                WindowState = priorWindowState;
+                fullScreenIsChecked = false;
+                pictureBoxFullScreen.Image = global::JibbaJabba.Properties.Resources.full_screen;
+            }
+
+            picture.Fit(maimImage, ref mainPictureBox);
+        }
+    }
+}
